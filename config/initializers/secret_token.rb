bed049ca5e830c64e80f2a93399e4a2a4fc14749
Shintaro Katafuchi <hot.chemistry21@gmail.com>
2012-01-08 08:07:54 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitmemo::Application.config.secret_token = 'f0f01d744c8c7d83228570e5d486b1aeba7adf5ecf0730601837a02457d3a2cdb188e443bbeff767d25364e794c1ac792ca1c4ba6ed2780bfb570a3a7d47f2e5'
