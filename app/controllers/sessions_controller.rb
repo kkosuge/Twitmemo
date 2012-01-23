b17aa048f434b698fdda540990088066c740bf01
kkosuge <ksgkzy@gmail.com>
2012-01-23 16:51:25 +0900

    ログインするたびにアイコンURL等更新する

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,19 @@
 class SessionsController < ApplicationController
   def login
     auth = request.env['omniauth.auth']
-    user = User.find_by_nickname(auth['user_info']['nickname']) || User.create_account(auth)
+
+    if user = User.find_by_nickname(auth['user_info']['nickname'])
+      user.img = auth['user_info']['image']
+      user.nickname = auth['user_info']['nickname']
+      user.token = auth['credentials']['token']
+      user.secret = auth['credentials']['secret']
+      user.save!
+
+      User.update_twitter_user(auth)
+    else 
+      User.create_account(auth)
+    end
+
     session[:user_id] = user.id
     session[:twitter_id] = user.twitter_id
     logger.debug auth.to_yaml
@@ -13,5 +25,9 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     session[:twitter_id] = nil
     redirect_to root_url, :notice => 'ログアウトしました。'
+  end
+
+  def failure
+    redirect_to root_url
   end
 end
