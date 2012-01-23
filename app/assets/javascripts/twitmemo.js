a031336c7fa12109216f66d8358fe841bdf173ad
Shintaro Katafuchi <hot.chemistry21@gmail.com>
2012-01-23 18:31:56 +0900

    18:31

--- app/assets/javascripts/twitmemo.js
+++ app/assets/javascripts/twitmemo.js
@@ -54,4 +54,13 @@ $(document).ready(function(){
 	 return false;
 	});
 
+      var api_url = "https://api.twitter.com/1/users/show.json?screen_name="+$("#input_screen_name").val()+"&callback=?";
+      $.getJSON(api_url, function(json,status){
+      $("#twitter_icon").html($("<img/>",{ src: json.profile_image_url , class:"service-profile-icon", style: "width:4em;height:4em;" }));
+      });
+
+
+
+
+
 });
