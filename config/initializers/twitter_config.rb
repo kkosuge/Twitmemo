b4908d69b16e86a32a3e3e22844c40b6f28a0014
kkosuge <ksgkzy@gmail.com>
2012-03-13 00:06:05 +0900

    Twitterのアプリケーションキーをconfig/twitter.ymlで設定するようにした

--- config/initializers/twitter_config.rb
+++ config/initializers/twitter_config.rb
@@ -1,4 +1,8 @@
+require "yaml"
+
 Twitmemo::Application.configure do 
-  config.consumer_key = ''
-  config.consumer_secret = ''
+  twitter = YAML.load(File.open("config/twitter.yml"))
+
+  config.consumer_key = twitter["consumer_key"]
+  config.consumer_secret = twitter["consumer_secret"]
 end
